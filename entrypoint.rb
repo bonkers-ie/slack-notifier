f5061c161ad37b4153fa01df5eb8f277cbbc2263
Ben Walsh <ben@dalymount.com>
2024-02-02 15:50:17 +0000

    Template

--- entrypoint.rb
+++ entrypoint.rb
@@ -15,14 +15,12 @@ end
 client = Slack::Web::Client.new
 client.auth_test
 
-options = (as_user: true, channel: channel)
+options = { channel:, text: }
 
 if ENV.key?('INPUT_TEMPLATE-FILE')
   args = YAML.safe_load(ENV['INPUT_TEMPLATE-ARGS']) if ENV.key?('INPUT_TEMPLATE-ARGS')
   options.merge!(ERB.new(File.read(ENV['INPUT_TEMPLATE-FILE'])))
 end
-
-options[:text] = text if text.present?
 
 response = client.chat_postMessage(options)
 raise response.error unless response.ok?
