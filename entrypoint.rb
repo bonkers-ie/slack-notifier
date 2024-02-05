0c65d9a9180a5e55a1a74d4bd952f99529c7e300
Ben Walsh <ben@dalymount.com>
2024-02-05 12:34:49 +0000

    Template

--- entrypoint.rb
+++ entrypoint.rb
@@ -5,22 +5,24 @@ require 'json'
 require 'slack-ruby-client'
 require 'yaml'
 
-channel = ENV['INPUT_CHANNEL'].gsub(/\A\W*/, '#')
-text = ENV['INPUT_TEXT']
-
 Slack.configure do |config|
   config.token = ENV['SLACK_API_TOKEN']
   raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
 end
 
+channel = ENV['INPUT_CHANNEL'].to_s.gsub(/\A\W*/, '#')
+template = ENV['INPUT_TEMPLATE']
+
+%i[channel template].each do { |var| raise "Missing '#{var}'! unless #{binding.local_variable_get(var).present?}" }
+
 client = Slack::Web::Client.new
 client.auth_test
 
-options = { channel:, text: }
+options = { channel: }
 
 if ENV.key?('INPUT_TEMPLATE')
   args = YAML.safe_load(ENV['INPUT_TEMPLATE'])
-  options.merge!(ERB.new(args.delete('erb')).result(binding))
+  options.merge!(ERB.new(args.delete('erb').to_s).result(binding))
 end
 
 response = client.chat_postMessage(options)
