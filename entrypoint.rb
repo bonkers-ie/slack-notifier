bdf14c5f9f5836573761d8db03a787e9c4485817
Ben Walsh <ben@dalymount.com>
2024-02-05 12:57:52 +0000

    Template

--- entrypoint.rb
+++ entrypoint.rb
@@ -9,22 +9,20 @@ Slack.configure do |config|
   config.token = ENV['SLACK_API_TOKEN']
   raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
 end
-
-channel = ENV['INPUT_CHANNEL'].to_s.gsub(/\A\W*/, '#')
-template = ENV['INPUT_TEMPLATE']
-
-%i[channel template].each do { |var| raise "Missing '#{var}'! unless #{binding.local_variable_get(var).present?}" }
-
 client = Slack::Web::Client.new
 client.auth_test
 
-options = { channel: }
+channel = ENV['INPUT_CHANNEL'].gsub(/\A\W*/, '#')
+options = YAML.safe_load(ENV['INPUT_OPTIONS'])
 
-if ENV.key?('INPUT_TEMPLATE')
-  args = YAML.safe_load(ENV['INPUT_TEMPLATE'])
-  options.merge!(ERB.new(args.delete('erb').to_s).result(binding))
+if ENV.key('INPUT_MESSAGE-ID')
+  # retrieve
+elsif options.key('erb')
+  slack_options = ERB.new(options.delete('erb').to_s).result(binding).merge(channel:)
+  slack_args[:metadata] = options
+  response = client.chat_postMessage(slack_options)
+  raise response.error unless response.ok?
+  puts "::set-output name=message-id::#{response.ts}"
+else
+  raise 'Must either provide a template or update an existing message'
 end
-
-response = client.chat_postMessage(options)
-raise response.error unless response.ok?
-puts "::set-output name=message-id::#{response.ts}"
