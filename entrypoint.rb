392ea6081a77aaf2ba9ab67e087ac563c9b42132
Ben Walsh <ben@dalymount.com>
2024-02-06 10:58:36 +0000

    --skip-ci

--- entrypoint.rb
+++ entrypoint.rb
@@ -15,18 +15,15 @@ end
 client = Slack::Web::Client.new
 client.auth_test
 
-channel = ENV['INPUT_CHANNEL'].gsub(/\A\W*/, '#')
+slack_options = { channel: ENV['INPUT_CHANNEL'].gsub(/\A\W*/, '#') }
 options = YAML.safe_load(ENV['INPUT_OPTIONS']).deep_symbolize_keys
 
-slack_options = { channel: }
 if ENV.key('INPUT_MESSAGE-ID')
-  # retrieve
+  oldest = ENV['INPUT_MESSAGE-ID']
+  puts client.conversations_history(slack_options.merge(inclusive: true, limit: 1, oldest:))
 elsif ENV['INPUT_TEMPLATE']
   slack_options.merge!(JSON.parse(ERB.new(ENV['INPUT_TEMPLATE'].to_s).result(binding)).deep_symbolize_keys)
   slack_options[:metadata] = options
-  puts '---------------'
-  puts slack_options
-  puts '---------------'
   response = client.chat_postMessage(slack_options)
   raise response.error unless response.ok?
   puts "::set-output name=message-id::#{response.ts}"
